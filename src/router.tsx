b0a9863360c90d0ec5a359e962a6b0829825e77f
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-30 14:09:29 -0600

    Added temporaly nav with auth

--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,37 @@
+import React, { FC } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
   Link
 } from "react-router-dom"
+import { connect } from 'react-redux'
+import { User } from './services/user.interface'
 import { DashboardScreen } from './views/dashboard.view'
 import { LoginScreen } from './views/login.view'
 import { SignupScreen } from './views/singup.view'
 import { PrivateRoute } from './components/privateRoute'
 import { OnlyPublicRoute } from './components/onlyPublicRoute'
 
-export const AppRouter = () => (
+interface State {
+  user: User | null | undefined
+}
+
+interface RouterProps {
+  user?: User | null | undefined
+}
+
+export const MainRouter:FC<RouterProps> = ({user}:RouterProps) => (
   <>
   <Router>
     <ul>
-      <li><Link to="/">Dashboard</Link></li>
-      <li><Link to="/login/">Login</Link></li>
-      <li><Link to="/signup/">Sign Up</Link></li>
+      {user ?
+        <li><Link to="/">Dashboard</Link></li>
+      :
+        <>
+          <li><Link to="/login/">Login</Link></li>
+          <li><Link to="/signup/">Sign Up</Link></li>
+        </>
+      }
     </ul>
     <Switch>
       <OnlyPublicRoute path="/signup/">
@@ -32,3 +47,14 @@ export const AppRouter = () => (
   </Router>
   </>
 )
+
+const mapStateToProps = (state:State) => {
+  return {
+    user: state.user
+  }
+}
+
+export const AppRouter = connect(
+  mapStateToProps,
+  null
+)(MainRouter)

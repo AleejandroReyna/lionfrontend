53cff38689ac853ed2d6f854210df6d18841d455
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-30 13:30:28 -0600

    Added basic nav menu

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,21 @@
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom"
 import { DashboardScreen } from './views/dashboard.view'
 import { LoginScreen } from './views/login.view'
 import { SignupScreen } from './views/singup.view'
 
 export const AppRouter = () => (
+  <>
   <Router>
+    <ul>
+      <li><Link to="/">Dashboard</Link></li>
+      <li><Link to="/login/">Login</Link></li>
+      <li><Link to="/signup/">Sign Up</Link></li>
+    </ul>
     <Switch>
       <Route path="/signup/">
         <SignupScreen />
@@ -21,4 +28,5 @@ export const AppRouter = () => (
       </Route>
     </Switch>
   </Router>
+  </>
 )

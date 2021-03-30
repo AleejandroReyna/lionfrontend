abb339d5fc64d5d62e76cef918314665ec583ef9
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-29 21:08:31 -0600

    Added favorite breed reducer

---
+++ src/reducers/favoriteBreed.reducer.tsx
@@ -0,0 +1,18 @@
+import { Breed } from '../services/breed.interface'
+import { SET_FAVORITE_BREED } from '../actions'
+
+const initialState:undefined | Breed[] = undefined
+
+interface Action {
+  type: string,
+  breed: Breed
+}
+
+export function breeds(state = initialState, action:Action) {
+  switch(action.type) {
+    case SET_FAVORITE_BREED:
+      return {...action.breed}
+    default:
+      return state
+  }
+}

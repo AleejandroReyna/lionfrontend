05f08bb0175c63a0ff6f946ca1498251fe746c58
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-31 08:17:36 -0600

    Added app welcome

--- src/components/breedInfo/index.tsx
+++ src/components/breedInfo/index.tsx
@@ -1,24 +1,44 @@
 import React, {useState, useEffect } from 'react'
-import { Card, Col, Container, Row } from 'react-bootstrap'
+import { Card, Col, Container, Row, Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { Breed } from '../../services/breed.interface'
+import { StarFill, ArrowRight, EyeFill, CaretDownFill, CaretUpFill } from 'react-bootstrap-icons'
 
 interface State {
   selectedBreed: Breed,
-  parentSelectedBreed: Breed
+  parentSelectedBreed: Breed,
+  favoriteBreed: Breed,
+  parentFavoriteBreed: Breed
 }
 
 interface Props {
   selectedBreed?: Breed,
-  parentSelectedBreed?: Breed
+  parentSelectedBreed?: Breed,
+  favoriteBreed?: Breed,
+  parentFavoriteBreed?: Breed
 }
 
-const Info = ({selectedBreed, parentSelectedBreed}:Props) => {
+const Info = ({selectedBreed, parentSelectedBreed, favoriteBreed, parentFavoriteBreed }:Props) => {
+  console.log(favoriteBreed, parentFavoriteBreed)
   
-  if(selectedBreed) {
+  if(selectedBreed || favoriteBreed) {
     return (
       <Container>
         <Row>
+          {favoriteBreed &&
+            <Col xs={4}>
+              <Card className="mt-4">
+                <Card.Body>
+                  <small><StarFill color="gold" /> Favorite Breed:</small>
+                  <h4>
+                    {parentFavoriteBreed &&
+                      <>{parentFavoriteBreed.name} <ArrowRight />{' '}</>
+                    }
+                    {favoriteBreed.name}</h4>
+                </Card.Body>
+              </Card>
+            </Col>
+          }
           {parentSelectedBreed &&
             <Col xs={4}>
               <Card className="mt-4">
@@ -29,25 +49,44 @@ const Info = ({selectedBreed, parentSelectedBreed}:Props) => {
               </Card>
             </Col>
           }
-          <Col xs={4}>
-            <Card className="mt-4">
-              <Card.Body>
-                <small>Selected Breed:</small>
-                <h4>{selectedBreed.name}</h4>
-              </Card.Body>
-            </Card>
-          </Col>
+          {selectedBreed &&
+            <Col xs={4}>
+              <Card className="mt-4">
+                <Card.Body>
+                  <small>Selected Breed:</small>
+                  <h4>{selectedBreed.name}</h4>
+                </Card.Body>
+              </Card>
+            </Col>
+          }
         </Row>
       </Container>
     )
   }
-  return null
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1 className="mt-4 mb-5">Welcome to Lion Breeds</h1>
+          <p>You can view dog breed photos when do you click the {' '}
+            <Button size="sm"><EyeFill /></Button> in the left sidebar.</p>
+          <p>If do you click in {' '}
+            <Button size="sm" variant="secondary"><CaretDownFill /></Button> sub breeds of breed are showed.</p>
+          <p>You can hide sub breeds with this button: <Button size="sm" variant="secondary"><CaretUpFill /></Button></p>
+          <p>You can select a favorite breed! Use this button <Button size="sm" variant="outline-secondary"><StarFill /></Button></p>
+          <p>That's it all, enjoy it!</p>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 const mapStateToProps = (state:State) => {
   return {
     selectedBreed: state.selectedBreed,
-    parentSelectedBreed: state.parentSelectedBreed
+    parentSelectedBreed: state.parentSelectedBreed,
+    favoriteBreed: state.favoriteBreed,
+    parentFavoriteBreed: state.parentFavoriteBreed
   }
 }
 

5a20ef3fe6ff352ada5d08866d2b311695d17435
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-31 10:04:50 -0600

    Verify if user is login when app loaded

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,21 +12,54 @@ import { SignupScreen } from './views/singup.view'
 import { PrivateRoute } from './components/privateRoute'
 import { OnlyPublicRoute } from './components/onlyPublicRoute'
 import { MainHeader } from './components/mainHeader'
+import { Dispatch } from 'redux'
+import { setUser } from './actions'
 
+interface LoginProps {
+  logUser(user: User): void
+}
 
-export const AppRouter:FC = () => (
-  <Router>
-    <MainHeader />
-    <Switch>
-      <OnlyPublicRoute path="/signup/">
-        <SignupScreen />
-      </OnlyPublicRoute>
-      <OnlyPublicRoute path="/login/">
-        <LoginScreen />
-      </OnlyPublicRoute>
-      <PrivateRoute path="/">
-        <DashboardScreen />
-      </PrivateRoute>
-    </Switch>
-  </Router>
-)
+export const MainRouter:FC<LoginProps> = ({logUser}:LoginProps) => {
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    let username = localStorage.getItem("username")
+    let token = localStorage.getItem("token")
+    if(username && token) {
+      logUser({username, token})
+    }
+    setLoading(false)
+  }, [loading])
+
+  if (loading) { return null }
+
+  return (
+    <Router>
+      <MainHeader />
+      <Switch>
+        <OnlyPublicRoute path="/signup/">
+          <SignupScreen />
+        </OnlyPublicRoute>
+        <OnlyPublicRoute path="/login/">
+          <LoginScreen />
+        </OnlyPublicRoute>
+        <PrivateRoute path="/">
+          <DashboardScreen />
+        </PrivateRoute>
+      </Switch>
+    </Router>
+  )
+}
+
+const mapDispatchToProps = (dispatch:Dispatch) => {
+  return {
+    logUser: (user:User) => {
+      dispatch(setUser(user))
+    }
+  }
+}
+
+export const AppRouter = connect(
+  null,
+  mapDispatchToProps
+)(MainRouter)

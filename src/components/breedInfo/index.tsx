32b2a21d4af79409e548549c4338ed025897a2fa
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-30 18:47:27 -0600

    Added parent name in Breed Info

--- src/components/breedInfo/index.tsx
+++ src/components/breedInfo/index.tsx
@@ -4,19 +4,31 @@ import { connect } from 'react-redux'
 import { Breed } from '../../services/breed.interface'
 
 interface State {
-  selectedBreed: Breed
+  selectedBreed: Breed,
+  parentSelectedBreed: Breed
 }
 
 interface Props {
-  selectedBreed?: Breed
+  selectedBreed?: Breed,
+  parentSelectedBreed?: Breed
 }
 
-const Info = ({selectedBreed}:Props) => {
+const Info = ({selectedBreed, parentSelectedBreed}:Props) => {
   
   if(selectedBreed) {
     return (
       <Container>
         <Row>
+          {parentSelectedBreed &&
+            <Col xs={4}>
+              <Card className="mt-4">
+                <Card.Body>
+                  <small>Parent Breed:</small>
+                  <h4>{parentSelectedBreed.name}</h4>
+                </Card.Body>
+              </Card>
+            </Col>
+          }
           <Col xs={4}>
             <Card className="mt-4">
               <Card.Body>
@@ -34,7 +46,8 @@ const Info = ({selectedBreed}:Props) => {
 
 const mapStateToProps = (state:State) => {
   return {
-    selectedBreed: state.selectedBreed
+    selectedBreed: state.selectedBreed,
+    parentSelectedBreed: state.parentSelectedBreed
   }
 }
 

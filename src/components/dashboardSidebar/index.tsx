71e17f05d5648640081f3760315fb78665fe7147
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-30 21:29:26 -0600

    filter by name

--- src/components/dashboardSidebar/index.tsx
+++ src/components/dashboardSidebar/index.tsx
@@ -1,7 +1,7 @@
 import React, {FC, useState, useEffect} from 'react'
 import { Breed } from '../../services/breed.interface'
 import { connect } from 'react-redux'
-import { Card, Spinner, ListGroup } from 'react-bootstrap'
+import { Card, Spinner, ListGroup, FormControl } from 'react-bootstrap'
 import {DashboardItem } from '../dashboardItem'
 import './index.css'
  
@@ -14,13 +14,32 @@ interface State {
 }
 
 const Sidebar:FC<SidebarProps> = ({ breeds }:SidebarProps) => {
+  const [textFilter, setTextFilter] = useState<string>("")
+
+  const getBreeds = ():Breed[] => {
+    if(breeds) {
+      if(textFilter) {
+        let firstFilter = breeds.filter(breed => breed.name.includes(textFilter))
+        return [...firstFilter]
+      }
+      return breeds
+    }
+    return []
+  }
   return (
     <Card className="mt-4 sidebar-card">
       <Card.Body>
-        {breeds ?
-          <ListGroup> 
-            { breeds?.map(breed => <DashboardItem key={breed.name} breed={breed} />) }
-          </ListGroup>
+        <FormControl 
+          placeholder="Ex:// akita"
+          value={textFilter}
+          onChange={(e) => {setTextFilter(e.target.value)}}
+        />
+        {getBreeds()?
+          <div className="list-group-container">
+            <ListGroup> 
+              { getBreeds().map(breed => <DashboardItem key={breed.name} breed={breed} />) }
+            </ListGroup>
+          </div>
         :
           <div className="text-center">
             <Spinner animation="border" variant="primary" />
